04d1a8560ccd4bb75a78a59a93f3aabb1690ddb0
abedul59 <70437442+abedul59@users.noreply.github.com>
2023-04-04 22:14:36 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -36,17 +36,45 @@ class ChatGPT:
 chatgpt = ChatGPT()
 
 
+class Prompt:
+
+    prompt_string: str
+    answer_string: str
+
+
+    def __init__(self):
+
+    def on_changed(self, symbol: str):
+        if symbol is None or symbol == "":
+            pass
+        if symbol.lower() == "generate":
+	    ai_reply_response = chatgpt.get_response(prompt_string)
+            self.answer_string = ai_reply_response
+		
+		
+
+
+
+prompt = Prompt()
+
+
 from nicegui.events import ValueChangeEventArguments
 
 
-def getGPTrespond(event: ValueChangeEventArguments):
-    ai_reply_response = chatgpt.get_response(event.value)
-    name = type(event.sender).__name__
-    ui.label(f'ChatGPT AI: {ai_reply_response}')
+#def getGPTrespond(event: ValueChangeEventArguments):
+    #ai_reply_response = chatgpt.get_response(event.value)
+    #name = type(event.sender).__name__
+    #ui.label(f'ChatGPT AI: {ai_reply_response}')
 
-ui.label('Please type in your question here first. And then cut and paste it below./ 先打好你的問題在這，然後貼到下面的空格。')    
-ui.input('Type in your Question!')    
-ui.label('Paste here, and get ChatGPT response./ 問題或提示貼在這裡，然後得到ChatGPT AI的回答。')    
-ui.input('Paste here', on_change=getGPTrespond)
+ui.input("Type in string here.").bind_value(prompt, "prompt_string")
 
+
+#ui.label('Please type in your question here first. And then cut and paste it below./ 先打好你的問題在這，然後貼到下面的空格。')    
+#ui.input('Type in your Question!')    
+#ui.label('Paste here, and get ChatGPT response./ 問題或提示貼在這裡，然後得到ChatGPT AI的回答。')    
+#ui.input('Paste here', on_change=getGPTrespond)
+
+ui.button("Generate", on_click=lambda: prompt.on_changed("generate"))
+
+ui.label(f'ChatGPT AI: {prompt.answer_string}').bind_value(prompt, "answer_string")
 ui.run()
