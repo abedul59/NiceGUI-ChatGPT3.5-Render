372e69186a1da34235785ec772b78a5ff3cd96c1
abedul59 <70437442+abedul59@users.noreply.github.com>
2023-04-04 19:57:24 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -44,9 +44,9 @@ def getGPTrespond(event: ValueChangeEventArguments):
     name = type(event.sender).__name__
     ui.label(f'ChatGPT AI: {ai_reply_response}')
 
-ui.label('Please type in your question here first. And then cut and paste it below./先打好你的問題在這，然後貼到下面的空格。')    
+ui.label('Please type in your question here first. And then cut and paste it below./ 先打好你的問題在這，然後貼到下面的空格。')    
 ui.input('Type in your Question!')    
-ui.label('Paste here, and get ChatGPT's response./ 問題或提示貼在這裡，然後得到ChatGPT AI的回答。')    
+ui.label('Paste here, and get ChatGPT response./ 問題或提示貼在這裡，然後得到ChatGPT AI的回答。')    
 ui.input('Paste here', on_change=getGPTrespond)
 
 ui.run()
