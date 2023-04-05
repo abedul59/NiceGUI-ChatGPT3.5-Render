1de44a2479d184a020c966b4129a1450545724f2
abedul59 <70437442+abedul59@users.noreply.github.com>
2023-04-05 11:51:22 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,6 +7,16 @@ import os, openai
 class OpenaiData:
     def __init__(self):
         self.api_key_string = ""    
+        self.api_key_string2 = "" 
+    def on_changed(self, symbol: str):
+        if symbol is None or symbol == "":
+            pass
+
+        elif symbol.lower() == "save_api":
+            print(self.api_key_string2)
+            self.api_key_string = self.api_key_string2
+
+
 
 odt = OpenaiData() #存放openai api key類別
 
@@ -77,6 +87,7 @@ class Prompt2Response:
 
 
 ui.input("Type in your openai api key here.").bind_value(odt, "api_key_string")
+ui.button("Save api key", on_click=lambda: odt.on_changed("save_api"))
 openai.api_key = odt.api_key_string #os.getenv("OPENAI_API_KEY")
 #ui.button("Save key first!", on_click=lambda: p2r.on_changed("save_api_key"))
 
